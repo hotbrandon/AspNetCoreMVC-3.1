c5483896bef2437ce48d2d1ca983f79e2ca44ca1
hotbrandon <hotbrandon@hotmail.com>
2021-02-23 16:08:10 +0800

    added home controller

--- WebGentle.BookStore/WebGentle.BookStore/Startup.cs
+++ WebGentle.BookStore/WebGentle.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace WebGentle.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,18 @@ namespace WebGentle.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("message from custom middleware1, incoming<br> ");
+            //    await next();
+            //    await context.Response.WriteAsync("message from custom middleware, outgoing<br>");
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
